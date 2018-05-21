e51c979d8fedfcbf608b514efb85601e56ee856f
c50a326 <c50a326@tutanota.com>
2018-05-21 15:03:57 +0545

    In the middle of adding typography.js.

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,11 @@ module.exports = {
     },
     `gatsby-plugin-react-svg`,
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfig: `src/utils/typography`
+      }
+    }
   ]
 }
